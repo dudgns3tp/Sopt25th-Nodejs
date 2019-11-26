72ec80bebff64de39dca1725bd3e1fe073310820
Choi Young Hoon <c_yh0327@naver.com>
2019-11-26 19:40:04 +0900

    books.js 작성

---
+++ homeWork/week6/model/books.js
@@ -0,0 +1,25 @@
+const statusCode = require('../module/statusCode');
+const responseMessage = require('../module/responseMessage');
+const authUtil = require('../module/authUtil');
+const pool = require('../module/pool');
+// 책장속 발견 
+const table = 'book';
+module.exports ={
+    selectAll : async() =>{
+        const query= `SELECT * FROM ${table}`;
+        const result = await pool.queryParam_None(query);
+    
+        if(!result){
+            return{
+                code:statusCode.BAD_REQUEST,
+                json:authUtil.successFalse(responseMessage.BOOK_READ_ALL_FAIL)
+            };
+        }
+
+        return{
+            code:statusCode.OK,
+            json:authUtil.successTrue(responseMessage.BOOK_READ_ALL_SUCCESS
+                )
+        };
+    }
+}
